a95c9be5a0da3e011ea9a4f073d18cd72d365736
Ava N <13421689+anederlander@users.noreply.github.com>
2024-04-11 15:02:58 -0400

    4/11 - new pages

--- app/src/main/java/com/example/speechinsight/MainActivity.java
+++ app/src/main/java/com/example/speechinsight/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    public void openChatGPTActivity(View view) {
+//        Intent intent = new Intent(this, ChatGPTActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openRecordActivity(View view) {
+//        Intent intent = new Intent(this, RecordActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openAnalyticsActivity(View view) {
+//        Intent intent = new Intent(this, AnalyticsActivity.class);
+//        startActivity(intent);
+//    }
 }
+
---
+++ app/src/main/java/com/example/speechinsight/RecordActivity.java
@@ -0,0 +1,4 @@
+package com.example.speechinsight;
+
+public class RecordActivity {
+}
