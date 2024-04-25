337990e77fe2f8ba2ca3ee76377007a570023015
Animesh-Uttekar <animeshuttekar28@gmail.com>
2024-04-25 18:32:43 -0400

    Speech Recognition update

--- app/src/main/java/com/example/speechinsight/MainActivity.java
+++ app/src/main/java/com/example/speechinsight/MainActivity.java
@@ -5,23 +5,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity{
+
+    private ImageView iv_mic;
+    private TextView tv_Speech_to_text;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_mic = findViewById(R.id.iv_mic);
+        tv_Speech_to_text = findViewById(R.id.tv_speech_to_text);
+
+        iv_mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent
+                        = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+
+                try {
+                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+                }
+                catch (Exception e) {
+                    Toast.makeText(MainActivity.this, " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> speech_to_text_data = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                tv_Speech_to_text.setText(Objects.requireNonNull(speech_to_text_data).get(0));
+            }
+        }
     }
 
 //    public void openChatGPTActivity(View view) {
 //        Intent intent = new Intent(this, ChatGPTActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openRecordActivity(View view) {
-//        Intent intent = new Intent(this, RecordActivity.class);
 //        startActivity(intent);
 //    }
 //
--- app/src/main/java/com/example/speechinsight/RecordActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.speechinsight;
-
-public class RecordActivity {
-}
